b93aa0b0aceb8872db7d26161d5c9cd995ca0f2e
sarantail <sarantail@gmail.com>
2023-08-30 19:51:44 +0300

    first commit

---
+++ 4/scr/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <LoanCalc />
+    </div>
+</template>
+
+<script>
+import LoanCalc from './components/LoanCalc.vue';
+
+export default {
+    name: 'App',
+    components: {
+        LoanCalc,
+    },
+};
+</script>
+
+<style></style>
