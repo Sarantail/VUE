c22a7c92ec635e97207019dd9bf7198450dfbab1
sarantail <sarantail@gmail.com>
2023-08-30 19:24:56 +0300

    first commit

---
+++ 3/blog_details.js
@@ -0,0 +1,206 @@
+Vue.component('Kitchen', {
+ template: `
+ <div class="blog__details_kitchen">
+ <div class="post__header">
+   Let’s Get Your Own New Kitchen
+ </div>
+ <img 
+   src="img/Blog_details_kitchen.svg"
+   alt="img/Blog_details_kitchen.svg"
+   class="blog__details__img"
+ />
+ <div class="blog__details__content">
+   <p class="small__text">28 Marzo,2023</p>
+   <div class="breadcrumb">
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="#">Interior</a>
+     </div>
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="index.html">Home</a>
+     </div>
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="index.html">Decor </a>
+     </div>
+     <!--закрытие   breadcrumb-->
+   </div>
+ </div>
+ <div class="blog__details__text">
+   <p class="normal">
+     Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+     turpmaximus.posuere in.Contrary to popular belief.
+   </p>
+ </div>
+</div>
+`, });
+Vue.component('Bedroom', { template: `
+<div class="blog__details_bedroom">
+              <div class="post__header">Design Your Nice Bedroom</div>
+              <p class="normal">
+                Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                turpmaximus.posuere in.Contrary to popular belief.There are many
+                variations of passages of Lorem Ipsum available, but the
+                majority have suffered.
+              </p>
+              <ol class="normal ol">
+                <li>
+                  Contrary to popular belief.There are many variations of
+                  passages of Lorem Ipsum available, but the majority have
+                  suffered.
+                </li>
+                <li>
+                  Contrary to popular belief.There are many variations of
+                  passages of Lorem Ipsum available, but the majority have
+                  suffered.
+                </li>
+              </ol>
+              <img
+                src="img/Blog_details_bedroom.svg"
+                alt="img/Blog_details_bedroom.svg"
+                class="blog__details__img"
+              />
+              <p class="normal">
+                Embarrassing hidden in the middle of text. All the Lorem Ipsum
+                generators on the Internet tend to repeat predefined chunks as
+                necessary.
+              </p>
+            </div>
+`, });
+Vue.component('Building', {
+ template: `
+ <div class="blog__details_kitchen_building">
+ <div class="post__header">
+   Let’s Get Solution for Building Construction Work
+ </div>
+ <img
+   src="img/Blog_details_building.svg"
+   alt="img/Blog_details_building.svg"
+   class="blog__details__img"
+ />
+ <div class="blog__details__content">
+   <p class="small__text">22 December,2022</p>
+   <div class="breadcrumb">
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="#">Interior</a>
+     </div>
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="index.html">Home</a>
+     </div>
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="index.html">Decor </a>
+     </div>
+   </div>
+ </div>
+ <div class="blog__details__text">
+   <p class="normal">
+     Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+     turpmaximus.posuere in.Contrary to popular belief.There are
+     many variations of passages of Lorem Ipsum available, but the
+     majority have suffered alteration in some form, by
+     injecthumour, or randomised words which don't look even
+     slightly believable.
+   </p>
+   <p class="normal">
+     Embarrassing hidden in the middle of text. All the Lorem Ipsum
+     generators on the Internet tend to repeat predefined chunks as
+     necessary.
+   </p>
+ </div>
+</div>
+`, });
+Vue.component('Architecture', { template: `
+<div class="blog__details_architecture">
+ <div class="post__header">
+   Architecture Is Our Pholosophy
+ </div>
+ <img
+   src="img/Blog_details_architecture.svg"
+   alt="img/Blog_details_architecture.svg"
+   class="blog__details__img"
+ />
+ <div class="blog__details__content">
+   <p class="small__text">22 December,2022</p>
+   <div class="breadcrumb">
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="#">Interior</a>
+     </div>
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="index.html">Home</a>
+     </div>
+     <div class="breadcrumb__box-link">
+       <a class="breadcrumb__link" href="index.html">Decor </a>
+     </div>
+   </div>
+ </div>
+ <div class="blog__details__text">
+   <p class="normal">
+     Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+     turpmaximus.posuere in.Contrary to popular belief.There are
+     many variations of passages of Lorem Ipsum available, but the
+     majority have suffered alteration in some form, by
+     injecthumour, or randomised words which don't look even
+     slightly believable.
+   </p>
+   <p class="normal">
+     Embarrassing hidden in the middle of text. All the Lorem Ipsum
+     generators on the Internet tend to repeat predefined chunks as
+     necessary.
+   </p>
+ </div>
+</div>
+`, });
+Vue.component('Kitchen_planning', {
+template: `
+<div class="blog__details_kitchen_panning">
+              <div class="blog__details__quote">
+                <svg
+                  width="74"
+                  height="56"
+                  viewBox="0 0 74 56"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M58.2516 0.600003C62.6516 0.600003 66.2516 2.06667 69.0516 5C71.7182 7.93334 73.0516 11.8667 73.0516 16.8C73.0516 25.4667 70.1849 33 64.4516 39.4C58.5849 45.8 51.5849 51.0667 43.4516 55.2L41.4516 51.8C44.7849 49.6667 47.9182 46.9333 50.8516 43.6C53.6516 40.2667 55.8516 36.6667 57.4516 32.8L51.8516 28.6C49.5849 27.1333 47.7182 25.1333 46.2516 22.6C44.7849 20.2 44.0516 17.8667 44.0516 15.6C44.0516 11.3333 45.3849 7.73334 48.0516 4.8C50.5849 2 53.9849 0.600002 58.2516 0.600003ZM17.0516 0.599999C21.4516 0.6 25.0516 2.06667 27.8516 5C30.5182 7.93333 31.8516 11.8667 31.8516 16.8C31.8516 25.4667 28.9849 33 23.2516 39.4C17.3849 45.8 10.3849 51.0667 2.25155 55.2L0.251554 51.8C3.58489 49.6667 6.71822 46.9333 9.65155 43.6C12.4516 40.2667 14.6516 36.6667 16.2516 32.8L10.6516 28.6C8.38489 27.1333 6.51822 25.1333 5.05155 22.6C3.58489 20.2 2.85156 17.8667 2.85156 15.6C2.85156 11.3333 4.18489 7.73333 6.85156 4.8C9.38489 2 12.7849 0.599999 17.0516 0.599999Z"
+                    fill="#CDA274"
+                  />
+                </svg>
+                <p class="blog__details__qoute__text">
+                  The details are not the details. They make the design.
+                </p>
+              </div>
+              <div class="post__header">Design sprints are great</div>
+              <p class="normal">
+                Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+                turpmaximus.posuere in.Contrary to popular belief.There are many
+                variations of passages of Lorem Ipsum available, but the
+                majority have suffered.
+              </p>
+              <ol class="normal ol">
+                <li>
+                  Contrary to popular belief.There are many variations of
+                  passages of Lorem Ipsum available, but the majority have
+                  suffered.
+                </li>
+                <li>
+                  Contrary to popular belief.There are many variations of
+                  passages of Lorem Ipsum available, but the majority have
+                  suffered.
+                </li>
+                <li>
+                  Contrary to popular belief.There are many variations of
+                  passages of Lorem Ipsum available, but the majority have
+                  suffered.
+                </li>
+              </ol>
+              <img
+                src="img/Blog_details_kitchen_planning.svg"
+                alt="img/Blog_details_kitchen_planning.svg"
+                class="blog__details__img"
+              />
+              <p class="normal">
+                Embarrassing hidden in the middle of text. All the Lorem Ipsum
+                generators on the Internet tend to repeat predefined chunks as
+                necessary.
+              </p>
+            </div>
+`, });
---
+++ 3/news_items.js
@@ -0,0 +1,56 @@
+const news_items = `[
+  {
+    "id": 0,
+    "img": "img/News1.svg",
+    "section_name": "Kitchen Design",
+    "section_href": "",
+    "item_text": "Let’s Get Solution For Building Construction Work",
+    "item_date": "26 December,2022",
+    "item_href": "blog_details.html"
+  },
+  {
+    "id": 1,
+    "img": "img/News2.svg",
+    "section_name": "Living Design",
+    "section_href": "",
+    "item_text": "Low Cost Latest Invented Interior Designing Ideas",
+    "item_date": "22 December,2022",
+    "item_href": "blog_details.html"
+  },
+  {
+   "id": 2,
+    "img": "img/News3.svg",
+    "section_name": "Interior Design",
+    "section_href": "",
+    "item_text": "Best For Any Office & Business Interior Solution",
+    "item_date": "25 December,2022",
+    "item_href": "blog_details.html"
+  },
+  {
+    "id": 3,
+    "img": "img/News4.svg",
+    "section_name": "Kitchen Design",
+    "section_href": "",
+    "item_text": "Let’s Get Solution For Building Construction Work",
+    "item_date": "26 December,2022",
+    "item_href": "blog_details.html"
+  },
+  {
+    "id": 4,
+    "img": "img/News5.svg",
+    "section_name": "Living Design",
+    "section_href": "",
+    "item_text": "Low Cost Latest Invented Interior Designing Ideas",
+    "item_date": "22 December,2022",
+    "item_href": "blog_details.html"
+  },
+  {
+    "id": 5,
+    "img": "img/News6.svg",
+    "section_name": "Interior Design",
+    "section_href": "",
+    "item_text": "Best For Any Office & Business Interior Solution",
+    "item_date": "25 December,2022",
+    "item_href": "blog_details.html"
+  }
+]`
